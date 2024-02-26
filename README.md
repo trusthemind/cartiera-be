9d9e489e7f7134c9ad484300026028991dab76f2
Fedir Melnyk <pluserwork24@gmail.com>
2024-02-26 12:34:39 +0200

    update for connections

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 How to run server with terminal
 
+run with Compile Daemon
 ```bash
 CompileDaemon -command="./go-cars-app"
+```
+---
+or run with command
+```bash
+go run main.go
 ```
--- initializers/connect.go
+++ initializers/connect.go
@@ -1,6 +1,8 @@
 package initializers
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"gorm.io/driver/postgres"
@@ -10,11 +12,34 @@ import (
 var DB *gorm.DB
 
 func ConnectToDB() {
-	var err error
-	dsn := os.Getenv("DB")
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	os.Setenv("PGHOST", "preferably-pretty-crayfish-iad.a1.pgedge.io")
+    os.Setenv("PGUSER", "admin")
+    os.Setenv("PGDATABASE", "cars_app_db")
+    os.Setenv("PGSSLMODE", "require")
+    os.Setenv("PGPASSWORD", "yvQRA68w032H2k4V07l4vPET")
 
-	if err != nil {
-		panic(err)
-	}
+    db, err := gorm.Open(postgres.New(postgres.Config{
+        DSN: fmt.Sprintf("host=%s user=%s dbname=%s sslmode=%s password=%s",
+            os.Getenv("PGHOST"), os.Getenv("PGUSER"), os.Getenv("PGDATABASE"), os.Getenv("PGSSLMODE"), os.Getenv("PGPASSWORD")),
+    }), &gorm.Config{})
+
+    if err != nil {
+        log.Fatalf("Failed to connect to database: %v", err)
+    }
+
+	DB = db
+
+    var nodeNames []string
+    if err := DB.Raw("SELECT node_name FROM spock.node").Scan(&nodeNames).Error; err != nil {
+        log.Fatalf("Failed to execute query: %v", err)
+    }
+
+	// For local connections
+	// var err error
+	// dsn := os.Getenv("DB")
+	// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	panic(err)
+	// }
 }
