68c6d3f70dcea40820863d5213dfb078c2c7861e
Fedir Melnyk <pluserwork24@gmail.com>
2024-06-03 22:52:01 +0300

    update (envs & routes) update env for connecting to db && update a returting route of user login

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -118,7 +118,7 @@ func Login(c *gin.Context) {
 	c.SetSameSite(http.SameSiteLaxMode)
 	c.SetCookie("Authorization", tokenString, 3600*24, "", "", true, true)
 
-	c.JSON(http.StatusOK, gin.H{"token": tokenString})
+	c.JSON(http.StatusOK, gin.H{"username": user.Name, "token": tokenString})
 }
 
 func Logout(c *gin.Context) {
--- initializers/connect.go
+++ initializers/connect.go
@@ -7,48 +7,32 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-
 )
 
 var DB *gorm.DB
 
 func ConnectToDB() {
-	os.Setenv("PGHOST", "rarely-precise-hen-iad.a1.pgedge.io")
-    os.Setenv("PGUSER", "admin")
-    os.Setenv("PGDATABASE", "cartiera_sales_db")
-    os.Setenv("PGSSLMODE", "require")
-    os.Setenv("PGPASSWORD", "424I4gTKsaE2np305KHx2U6H")
-
 	db, err := gorm.Open(postgres.New(postgres.Config{
-	    DSN: fmt.Sprintf("host=%s user=%s dbname=%s sslmode=%s password=%s",
-	        os.Getenv("PGHOST"), os.Getenv("PGUSER"), os.Getenv("PGDATABASE"), os.Getenv("PGSSLMODE"), os.Getenv("PGPASSWORD")),
+		DSN: fmt.Sprintf("host=%s user=%s dbname=%s sslmode=%s password=%s",
+			os.Getenv("PGHOST"), os.Getenv("PGUSER"), os.Getenv("PGDATABASE"), os.Getenv("PGSSLMODE"), os.Getenv("PGPASSWORD")),
 	}), &gorm.Config{})
 
 	if err != nil {
-	    log.Fatalf("Failed to connect to database: %v", err)
+		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
 	DB = db
 
 	var nodeNames []string
 	if err := DB.Raw("SELECT node_name FROM spock.node").Scan(&nodeNames).Error; err != nil {
-	    log.Fatalf("Failed to execute query: %v", err)
+		log.Fatalf("Failed to execute query: %v", err)
 	}
-
-	// For local connections
-	// var err error
-	// dsn := os.Getenv("DB")
-	// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	panic(err)
-	// }
 }
 
 func CloseDB() {
-	slqDB, err := DB.DB()
+	db, err := DB.DB()
 	if err != nil {
 		panic(err)
 	}
-	slqDB.Close();
+	db.Close()
 }
